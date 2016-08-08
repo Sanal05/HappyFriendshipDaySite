adb625c9c9ac053a6719ca8e0a4edbdd37fd51f6
Santosh Dora <sanal5hd@gmail.com>
2016-08-08 12:12:42 +0530

    Happy Friendship Day

---
+++ js/myjs.js
@@ -0,0 +1,7 @@
+ $(document).ready(function(){
+ 	    $('.parallax').parallax();
+      
+      $('.carousel.carousel-slider').carousel({full_width: true});
+      
+    });
+ 
---
+++ js/songjs.js
@@ -0,0 +1,9 @@
+var vid = document.getElementById("myAudio");
+
+function playVid() {
+    vid.play();
+}
+
+function pauseVid() {
+    vid.pause();
+}
